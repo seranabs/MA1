c977fc756bbe28a32842b1018c1c3d2ba14e10ca
Sara <snabila.2505@gmail.com>
2022-12-07 10:17:17 +0800

    Update level1.js

--- AgentApplejack/level1.js
+++ AgentApplejack/level1.js
@@ -304,13 +304,13 @@ class level1 extends Phaser.Scene {
     //this.physics.add.collider(mathis.player, this.this.player);
 
 
-    // this.physics.add.overlap(
-    //   this.player,
-    //   this.e2,
-    //   this.hitEnemy,
-    //   null,
-    //   this
-    // );
+    this.physics.add.overlap(
+      this.player,
+      this.e2,
+      this.hitEnemy,
+      null,
+      this
+    );
 
     this.physics.add.overlap(
       this.player,
@@ -319,6 +319,8 @@ class level1 extends Phaser.Scene {
       null,
       this
     );
+
+    
 
     this.physics.add.overlap(
       this.player,
